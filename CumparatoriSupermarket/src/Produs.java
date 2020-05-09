d094ad26af0450097754608a84954ff4cf606a3c
C-tinAurel <c_tin.aurel@yahoo.com>
2020-05-09 20:18:16 +0300

    Am modificat

---
+++ CumparatoriSupermarket/src/Produs.java
@@ -0,0 +1,25 @@
+public class Produs {
+    protected String nume;
+    protected int pret;
+
+    public Produs() {
+
+    }
+
+    public Produs(String nume, int pret) {
+        this.nume = nume;
+        this.pret = pret;
+    }
+
+    public String getNume() {
+
+        return nume;
+    }
+
+
+    public int getPret() {
+
+        return pret;
+    }
+}
+
